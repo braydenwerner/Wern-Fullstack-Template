b00032ffb87b217880d58cbe0653f861c44a2409
Brayden Werner <braydenwerner0@gmail.com>
2021-05-29 18:35:06 -0700

    Updated frontend config

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,5 +1,9 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const URL = dev ? 'http://localhost:3000/api' : ''
+export const URL = dev
+  ? 'http://localhost:3000/api'
+  : 'https://fullstack-wern-boilerplate.vercel.app'
 
-export const serverURL = dev ? 'http://localhost:4000' : ''
+export const serverURL = dev
+  ? 'http://localhost:4000'
+  : 'https://wern-fullstack-template.herokuapp.com/'
