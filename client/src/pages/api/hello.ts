3a5e966a555591e517b5191635c06ad7b10dbf9d
Brayden Werner <braydenwerner0@gmail.com>
2021-05-29 18:42:10 -0700

    Fixed deployment errors

--- client/src/pages/api/hello.ts
+++ client/src/pages/api/hello.ts
@@ -1,6 +1,4 @@
-// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-// export default (req, res) => {
-//   res.status(200).json({ name: 'John Doe' })
-// }
-export {}
+export default (req: any, res: any) => {
+  console.log(req)
+  res.status(200).json({ name: 'John Doe' })
+}
